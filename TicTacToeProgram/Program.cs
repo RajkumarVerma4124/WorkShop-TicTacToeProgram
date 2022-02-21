d126dff3dde1b3fe2449fae398396f9d265e0dfa
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-21 09:52:22 +0530

    [Raj] Added:- The code for fresh start for player

---
+++ TicTacToeProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeProgram
+{
+    /// <summary>
+    /// Main Program
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Displaying Welcome Message
+            Console.WriteLine("Welcome to the tic toe program");
+
+            //Calling the tictactoegame start fresh method
+            TicTacToeGame.StartFresh();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TicTacToeProgram/TicTacToeGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeProgram
+{
+    /// <summary>
+    /// TicTacToe Game Program
+    /// </summary>
+    public class TicTacToeGame
+    {
+        // static variables
+        public static char[] playBoard;
+
+        //Method for player to start fresh by resetting the board
+        public static void StartFresh()
+        {
+            Console.WriteLine("==================");
+            Console.WriteLine("||              ||");
+            Console.WriteLine("|| Game Started ||");
+            Console.WriteLine("||              ||");
+            Console.WriteLine("==================");
+            playBoard = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+            Console.ReadLine();
+        }
+    }
+}
